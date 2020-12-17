604f21d7abe844bbb7da10fdae9b928ad019582b
Haimeng Wang <hwang22@nd.edu>
2020-12-17 11:32:45 -0500

    pytest update

---
+++ molecool/tests/test_molecule.py
@@ -0,0 +1,14 @@
+"""
+Test for molecule module
+"""
+import pytest
+import molecool
+
+def test_molecule_mass():
+    symbols = ['C', 'H', 'H', 'H', 'H']
+
+    calculated_mass = molecool.calculate_molecular_mass(symbols)
+
+    actual_mass = 16.04 
+
+    assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass

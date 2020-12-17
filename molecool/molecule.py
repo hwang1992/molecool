af77d218b641bc4436b53da978eb3e825cea0131
Haimeng Wang <hwang22@nd.edu>
2020-12-17 13:37:05 -0500

    update pytest

--- molecool/molecule.py
+++ molecool/molecule.py
@@ -2,6 +2,7 @@
 Functions for analyzing/measuring molecules
 """
 from .measure import calculate_distance
+from .atom_data import atomic_weights 
 
 def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
     
@@ -15,3 +16,22 @@ def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
             if distance > min_bond and distance < max_bond:
                 bonds[(atom1, atom2)] = distance
 
+
+def calculate_molecular_mass(symbols):
+    """Calculate the mass of a molecule.
+          
+    Parameters
+    ----------
+    symbols : list
+    A list of elements.
+                             
+    Returns
+    -------
+    mass : float
+    The mass of the molecule
+    """
+    mass = 0.0
+    for atom in symbols:
+        mass += atomic_weights[atom]
+    return mass
+    
--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -4,6 +4,7 @@ Tests for the measure module
 
 import molecool as mol
 import numpy as np
+import pytest
 
 def test_calculate_distance():
 
@@ -14,3 +15,13 @@ def test_calculate_distance():
     calculate_dist = mol.calculate_distance(r1, r2)
 
     assert expect_dist == calculate_dist
+
+def test_angle():
+    r1 = np.array([0.0, 0.0, -1.0])
+    r2 = np.array([0.0, 0.0, 0.0])
+    r3 = np.array([1.0, 0.0, 0.0])
+
+    expect_value = 90.0
+    calculate_value = mol.calculate_angle(r1, r2, r3, degrees=True)
+
+    assert pytest.approx(expect_value, abs=1e-2) == calculate_value
